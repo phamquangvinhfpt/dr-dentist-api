be07aabd710341dd525d3958d2c8ab183ff424c8
Đoàn Công Toàn <134987653+ToanDC28@users.noreply.github.com>
2024-10-12 18:35:44 +0700

    create, update, delete, get medical history

---
+++ src/Core/Application/Identity/MedicalHistories/IMedicalHistoryService.cs
@@ -0,0 +1,14 @@
+﻿using FSH.WebApi.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.WebApi.Application.Identity.MedicalHistories;
+public interface IMedicalHistoryService : ITransientService
+{
+    Task<MedicalHistory> GetMedicalHistoryByPatientID(string patientID, CancellationToken cancellationToken);
+    Task CreateAndUpdateMedicalHistory(CreateAndUpdateMedicalHistoryRequest request, CancellationToken cancellationToken);
+    Task<string> DeleteMedicalHistory(string patientID, CancellationToken cancellationToken);
+}
